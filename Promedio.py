5b104820edae4b27485d0c802c02cef63b856c01
Jhojanops <espinjhojan@gmail.com>
2025-03-18 22:07:38 -0500

    Add files via upload

---
+++ Promedio.py
@@ -0,0 +1,6 @@
+nota1=float(input("ingrese la primera "))
+nota2=float(input("ingrese la segunda "))
+nota3=float(input("ingrese la tercera "))
+
+promedio=(nota1+nota2+nota3)/3
+print("el promedio es:",promedio)
